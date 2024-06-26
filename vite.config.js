ec8d289e3d1c1332975351385223cf43e8d7e136
Arifa <arifaiffat1995@gmail.com>
2024-06-27 02:03:09 +0530

    changes in img path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/My_Portfolio_In_React/'
+  base: '/My_Portfolio_In_React/',
+  plugins: [react()]
 })
