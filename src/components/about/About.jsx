943f47f42f3ac11ddaad73f69f70b1e55bda0bf1
Arifa <arifaiffat1995@gmail.com>
2024-06-27 00:57:22 +0530

    few changes in src

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
             <div className='aboutSec_2'>
                     {/* <img src={`${process.env.PUBLIC_URL}/media/img/headerImg-500x500.png`} alt='headerimg' className='headerImg' /> */}
                 
-                    <img src={`${process.env.PUBLIC_URL}/media/img/headerImg-500x500.png`} alt='headerimg' className='headerImg' />
+                    <img src="My_Portfolio_In_React/media/img/headerImg-500x500.png" alt='headerimg' className='headerImg' />
 
                 
                 </div>
