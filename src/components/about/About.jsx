318cef414b558157244372622610cadf934ee1a8
Arifa <arifaiffat1995@gmail.com>
2024-07-01 02:20:19 +0530

    some changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -28,9 +28,9 @@ const About = () => {
             </div>
                 
             <div className='aboutSec_2'>
-                {/* <img src={`${process.env.PUBLIC_URL}/media/img/headerImg-500x500.png`} alt='headerimg' className='headerImg' /> */}
+                <img src={`${process.env.PUBLIC_URL}/media/img/headerImg-500x500.png`} alt='headerimg' className='headerImg' />
                 
-                <img src="../../../public/media/headerImg-500x500.png" alt='headerimg' className='headerImg' />
+                {/* <img src="../../../public/media/headerImg-500x500.png" alt='headerimg' className='headerImg' /> */}
 
                 
                 </div>
