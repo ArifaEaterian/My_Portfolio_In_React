0c3c4c9fe0fa831351dd653b01be49718038f0db
Arifa <arifaiffat1995@gmail.com>
2024-07-01 19:22:51 +0530

    Changes in certificates

--- src/components/certification/Certification.jsx
+++ src/components/certification/Certification.jsx
@@ -27,7 +27,7 @@ const Certification = () => {
                 </div>
                 <div className='cert_col'>
                     <img src='https://arifaeaterian.github.io//My_Portfolio_In_React/assets/images/Lakshya_Oracle.png' alt='internship' />
-                    <h5 className='cert_head'>Oracle (MySQL)</h5>
+                    <h5 className='cert_head'>Oracle 10G (MySQL)</h5>
                     <p className='cert_dt'>May 2017 - Jun 2017</p>
                 </div>
             </div>
